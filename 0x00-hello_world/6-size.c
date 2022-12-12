cb0eedd573c28407fbfcc6b433347fa76df20dcb
Kelubia <kelubiaurubusi@yahoo.com>
2022-12-12 10:37:45 -0800

    size.c code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ int main(void)
 	printf("Size of an int: %zu byte(S)\n",	sizeof(int));
 	printf("Size of a long int: %zu byte(S)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(S)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(S)\n", sizeof(float));
+	printf("Size of a float: %i byte(S)\n", sizeof(float));
 	return (0);
 }
